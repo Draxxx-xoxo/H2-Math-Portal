ac3283197f07c1ba82dc26c6aaff26d752691d37
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2024-07-18 23:59:10 +0800

    Stripe Subscriptions

--- api/index.py
+++ api/index.py
@@ -1,12 +1,16 @@
-from flask import Flask, redirect, render_template, session, url_for, flash
+from flask import Flask, redirect, render_template, session, url_for, flash, jsonify
 import csv
 import json
 import os
+import requests
+from requests.auth import HTTPBasicAuth
 from datetime import datetime
 from supabase import create_client, Client
 from functools import wraps  # Import wraps decorator
 from api.routes.dashboard import dashboard
 from api.routes.auth import auth
+import stripe
+stripe.api_key = os.environ.get("STIPE_API_KEY")
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
@@ -36,6 +40,45 @@ def authorization_required(f):
 @app.route('/')
 def home():
     return render_template('dashboard.html')
+
+@app.route('/pricing')
+def pricing():
+    print(session['user'])
+    return render_template('stripe_pt.html', email=session['user'] )
+
+
+@app.route('/create-customer-portal-session', methods=['POST'])
+def customer_portal():
+
+    try:
+        response = requests.get(
+            'https://api.stripe.com/v1/customers',
+            params={'email': session['user']},
+            headers={'Authorization': f'Bearer {os.environ.get("STIPE_API_KEY")}'}
+        )
+
+        if response.status_code == 200:
+            results = response.json()
+
+            if 'data' in results and len(results['data']) > 0:
+                customer_id = results['data'][0]['id']
+
+                portal_session = stripe.billing_portal.Session.create(
+                    customer=customer_id,
+                    return_url="https://supabase-flask.vercel.app/dashboard",
+                )
+
+                return redirect(portal_session.url)
+
+            else:
+                return jsonify({'error': 'No customer found with the provided email'}), 404
+
+        else:
+            return jsonify({'error': 'Failed to retrieve customer data from Stripe'}), response.status_code
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
