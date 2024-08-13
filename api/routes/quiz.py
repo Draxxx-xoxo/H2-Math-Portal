e409bb3c863be50c40abe8003c5d4031080519fc
jonesbang <35623215+Draxxx-xoxo@users.noreply.github.com>
2024-08-13 20:23:26 +0800

    Stops from resetting the time again and again

--- api/routes/quiz.py
+++ api/routes/quiz.py
@@ -41,7 +41,10 @@ def dashboard_route(quiz_id):
 @authorization_required
 def start(quiz_id, session_id):
 
-    return render_template('start_quiz.html', quiz_id=quiz_id, session_id=session_id, title="Start Quiz")
+    res = supabase.table("session_quiz").select("is_active").eq("session_id", session_id).execute()
+    is_active = res.data[0]['is_active']
+
+    return render_template('start_quiz.html', quiz_id=quiz_id, session_id=session_id, is_active=is_active ,title="Start Quiz")
 
 # Initalise Timer    
 @quiz.route('/quiz/<quiz_id>/<session_id>/initalise', methods=["GET", "POST"])
@@ -80,3 +83,15 @@ def question_submit(question_no, quiz_id, session_id):
     
     return redirect(url_for('quiz.quiz_question', question_no=int(question_no), quiz_id=quiz_id, session_id=session_id))
 
+
+@quiz.route('/quiz/<quiz_id>/<session_id>/end', methods=["GET"])
+@authorization_required
+def end(quiz_id, session_id):
+    
+        res = supabase.table("session_quiz").select("is_active").eq("session_id", session_id).execute()
+        is_active = res.data[0]['is_active']
+    
+        if is_active == True:
+            supabase.table("session_quiz").update({"is_active": False, "is_completed": True}).eq("session_id", session_id).execute()
+    
+        return redirect(url_for('dashboard.dashboard_route'))
